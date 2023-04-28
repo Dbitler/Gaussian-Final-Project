7bdf399239ade5d8f0387009246f017e0f7a02a1
Dbitler <dbitler@hawk.iit.edu>
2023-04-27 19:01:18 -0500

    Initial Commit

---
+++ Gaussian Final Project/Gaussian_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gaussian_Final_ProjectApp.swift
+//  Gaussian Final Project
+//
+//  Created by IIT PHYS 440 on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Gaussian_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
