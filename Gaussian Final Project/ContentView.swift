4aa0bf140cd01d821637ac207c4e4eb87d900f43
Dbitler <dbitler@hawk.iit.edu>
2023-05-04 18:22:17 -0500

    trying some things out.

--- Gaussian Final Project/ContentView.swift
+++ Gaussian Final Project/ContentView.swift
@@ -6,12 +6,10 @@
 //
 // Main problems left to solve:
 // ----------------------------
-// have to optimize the path-finding code for the graph. X
-// have to make the graph only visible one the button is pressed.
-// should make it so the variables are easily editable. 
 // need to make the graphs update when variables are edited and the button is pressed.
-//fix strange bug that removes normalization when run through the least squares fitting.
 // call to testingGaussian is unused?? what????
+// the baseline graph function refuses to graph, despite the function clearly being called upon????
+// graph changes to ellipsoidal when the string is changed in the code, but not when it is changed in the text box. 
 // That's it, i guess????
 
 import SwiftUI
@@ -31,8 +29,6 @@ struct ContentView: View {
     
     let Width = 15
     let Height = 15
-    //want the color to vary with intensity, with red being at the peak and blue being at the nadir.
-   // @State var GColor = Color(red: 0.25, green: 0.5, blue: 0.75)
     var body: some View {
         VStack{
             HStack{
@@ -94,7 +90,7 @@ struct ContentView: View {
                 }
             }
             HStack{
-                Canvas{ context, size in gaussFinder.BaselineTestGaussian(Width: 15, Height: 15)
+                Canvas{ context, size in gaussFinder.BaselineTestGaussian()
                     for value in gaussFinder.intensities{
                         let rect = CGRect(x: value.x * (size.width/CGFloat(Width)), y: value.y * (size.height/CGFloat(Height)), width: (size.height/CGFloat(Width)), height: (size.height/CGFloat(Height)))
                         let shape = Rectangle().path(in: rect)
@@ -128,8 +124,14 @@ struct ContentView: View {
             }
             .padding()
             VStack{
-                Button("Click for Least Squares", action: TestingFunction)
-                Button("Click for Graph", action: self.setupSpins)
+                Button("Click for Least Squares", action: {TestingFunction()
+                    gaussFinder.Test = true
+                    
+                    
+                })
+                Button("Click for Graph", action: {GraphTest()
+                    gaussFinder.Baseline = true
+                })
             }
                     
         }
@@ -140,14 +142,14 @@ struct ContentView: View {
     func TestingFunction(){
         mygaussianinstance.TestingGaussian()
     }
-//    func GraphTest(){
-//        gaussFinder.BaselineTestGaussian()
-//    }
-    func setupSpins(){
-        
-        gaussFinder.BaselineTestGaussian(Width: Int(Width), Height: Int(Height))
-        
+    func GraphTest(){
+        gaussFinder.BaselineTestGaussian()
     }
+//    func setupSpins(){
+//
+//        gaussFinder.BaselineTestGaussian(Width: Int(Width), Height: Int(Height))
+//
+//    }
 }
 struct Intensity: Hashable, Equatable {
     var x: Double
@@ -162,26 +164,33 @@ class TestGaussianFinder: ObservableObject{
     var fittingintensities: [(x:Double, y:Double, intensity:Double)] = []
     var mygaussianinstance = GaussianFinder()
     var myequationinstance = BaselineGaussianEquations()
+    
+    var Test = false
+    var Baseline = false
+  
+   
 
-    func BaselineTestGaussian(Width: Int, Height: Int){
+    func BaselineTestGaussian(){
         intensities = []
-        
-        let I_0 = Double(mygaussianinstance.Itruestring)!
-        let width = 15
-        let height = 15
-        //let Intensity = 1.5
-        let sigma_x = Double(mygaussianinstance.Sxtruestring)!
-        let sigma_y = 1.3
-        let x_0 = 7.0
-        let y_0 = 7.0
-        let A = 0.006
-        let B = 0.006
-        var normalizefactor = 0.0
-        var positivefactor = 0.0
-        for x in 0..<Width{
-                for y in 0..<Height{
-                    let value = myequationinstance.Gaussianeqn(x: x, y: y, x_0: x_0, y_0: y_0, sigma_x: sigma_x, sigma_y: sigma_y, I_0: I_0, A: A, B: B)
-                   // intensities.append(Intensity(x: Double(x), y: Double(y), intensity: Double(value)))
+        //This only comes on if i press Graph then do least squares??? waht the fuck??????????
+        //if Baseline == true {
+            print("hello")
+            let I_0 = Double(mygaussianinstance.Itruestring)!
+            let width = 15
+            let height = 15
+            //let Intensity = 1.5
+            let sigma_x = Double(mygaussianinstance.Sxtruestring)!
+            let sigma_y = Double(mygaussianinstance.Sytruestring)!
+            let x_0 = Double(mygaussianinstance.xtruestring)!
+            let y_0 = Double(mygaussianinstance.ytruestring)!
+            let A = Double(mygaussianinstance.Ateststring)!
+            let B = Double(mygaussianinstance.Bteststring)!
+            var normalizefactor = 0.0
+            var positivefactor = 0.0
+            for x in 0..<width{
+                for y in 0..<height{
+                    let value = myequationinstance.Gaussianeqn(x: x, y: y, x_0: x_0, y_0: y_0, sigma_x: sigma_x, sigma_y: sigma_y, I_0: 5.0, A: A, B: B)
+                    // intensities.append(Intensity(x: Double(x), y: Double(y), intensity: Double(value)))
                     if value > normalizefactor{
                         normalizefactor = value
                     }
@@ -192,42 +201,44 @@ class TestGaussianFinder: ObservableObject{
                     //intensities.append(Intensity(x: Double(x), y: Double(y), intensity: Double(value)))
                     //have to normalize the values, such that the maximum value in the graph is =1, so that the colors of the graph look alright.
                 }
-        }
-            // this normalizes the data, such that the data is all positive and between 0 and 1, thus preventing the color code from peaking out and flattening the curve.
+            }
+            // this normalizes the data, such that the data is all positive and between 0 and 1, thus preventing the color code from peaking out and flattening the curve. has the side effect of making every graph look the same.
             for y in 0..<(height*width){
                 intensities[y].2 = Double(intensities[y].intensity) - positivefactor
                 intensities[y].2 = Double(intensities[y].intensity) / normalizefactor
             }
-
-     //print(intensities)
+            
+            //print(intensities)
+        
     }
     
     func FittingGaussian(){
         fittingintensities = []
-        let width = 15
-        let height = 15
-        var normalizefactor_fit = 0.0
-        var positivefactor_fit = 0.0
-        let foundParameters :[Double] = mygaussianinstance.TestingGaussian()
-        for x in 0..<width{
-            for y in 0..<height{
-                let value = myequationinstance.Gaussianeqn(x: x, y: y, x_0: foundParameters[5], y_0: foundParameters[6], sigma_x: foundParameters[1], sigma_y: foundParameters[2], I_0: foundParameters[0], A: foundParameters[3], B: foundParameters[4])
-                if value > normalizefactor_fit{
-                    normalizefactor_fit = value
+        if Test == true {
+            let width = 15
+            let height = 15
+            var normalizefactor_fit = 0.0
+            var positivefactor_fit = 0.0
+            let foundParameters :[Double] = mygaussianinstance.TestingGaussian()
+            for x in 0..<width{
+                for y in 0..<height{
+                    let value = myequationinstance.Gaussianeqn(x: x, y: y, x_0: foundParameters[5], y_0: foundParameters[6], sigma_x: foundParameters[1], sigma_y: foundParameters[2], I_0: foundParameters[0], A: foundParameters[3], B: foundParameters[4])
+                    if value > normalizefactor_fit{
+                        normalizefactor_fit = value
+                    }
+                    if value < positivefactor_fit{
+                        positivefactor_fit = value
+                    }
+                    //intensities.append(Intensity(x: Double(x), y: Double(y), intensity: Double(value))
+                    fittingintensities.append((x: Double(x), y: Double(y), intensity: Double(value)))
+                    //need to make this return to the GaussFinder, so that i can be displayed in the graph proper.
                 }
-                if value < positivefactor_fit{
-                    positivefactor_fit = value
-                }
-                //intensities.append(Intensity(x: Double(x), y: Double(y), intensity: Double(value))
-                fittingintensities.append((x: Double(x), y: Double(y), intensity: Double(value)))
-                //need to make this return to the GaussFinder, so that i can be displayed in the graph proper.
+            }
+            for y in 0..<(height*width){
+                fittingintensities[y].2 = Double(fittingintensities[y].intensity) - positivefactor_fit
+                fittingintensities[y].2 = Double(fittingintensities[y].intensity) / normalizefactor_fit
             }
         }
-        for y in 0..<(height*width){
-            fittingintensities[y].2 = Double(fittingintensities[y].intensity) - positivefactor_fit
-            fittingintensities[y].2 = Double(fittingintensities[y].intensity) / normalizefactor_fit
-        }
-
     }
 
 }
--- GaussianFinder.swift
+++ GaussianFinder.swift
@@ -22,7 +22,8 @@ class GaussianFinder: ObservableObject {
     
     @Published var Iteststring = "1.0"
     @Published var Itruestring = "1.0"
-
+    
+// graph changes upon start up to ellipsoidal when these are changed in the code, but they don't change when they are edited in text box. 
     @Published var Sxteststring = "1.3"
     @Published var Syteststring = "1.3"
     
@@ -50,6 +51,7 @@ class GaussianFinder: ObservableObject {
         let height = 15
         var numloop = 0.0
        // var Intensity = 1.5
+        //something is wrong with the algorithm for sigma x and sigma y, I don't know what. Sigma x is unable to find values smaller than initial value, and sigma y is unable to find values larger than initial value. 
         var Sigmax = Double(Sxteststring)!
         var Sigmay = Double(Syteststring)!
         let sigx_true = Double(Sxtruestring)!
@@ -71,23 +73,98 @@ class GaussianFinder: ObservableObject {
         var minimized = true
         let TestingValues = myequationinstance.TiltedGaussian(I_0: I_true, A: Atrue, B: Btrue, x_0: x_0true, y_0: y_0true, sigma_x: sigx_true, sigma_y: sigy_true, width: width, height: width)
         //this is the Intensity that the function below is testing against, to find the least squares. In the IPRO code, this would be removed,and it would be instead testing against the raw data.
+        
+        let h = 0.01
+        let k = 0.01
+        let f = 0.01
+        let l = 0.01
+        let m = 0.01
+        let n = 0.0001
+        let o = 0.0001
+        
+        let Istep = h
+        let Sxstep = k
+        let Systep = l
+        let x0step = f
+        let y0step = m
+        let Astep = n
+        let Bstep = o
+        
         while minimized{
             //3x3x3x3x3x3x3 matrix
-          // var SumArray = Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: 0.0, count: 3), count: 3), count: 3), count: 3), count: 3), count: 3), count: 3)
-            let h = 0.01
-            let k = 0.01
-            let f = 0.01
-            let l = 0.01
-            let m = 0.01
-            let n = 0.0001
-            let o = 0.0001
-            var sum = 0.0
-           let SumArray = mysuminstance.Sumfinder(Intensity: Intensity, A: A, B: B, x_0: x_0, y_0: y_0, Sigmax: Sigmax, Sigmay: Sigmay, width: width, height: height, Istep: h, Sxstep: k, Systep: l, x0step: f, y0step: m, Astep: n, Bstep: o, TestingValues: TestingValues)
+          // let SumArray = mysuminstance.Sumfinder(Intensity: Intensity, A: A, B: B, x_0: x_0, y_0: y_0, Sigmax: Sigmax, Sigmay: Sigmay, width: width, height: height, Istep: h, Sxstep: k, Systep: l, x0step: f, y0step: m, Astep: n, Bstep: o, TestingValues: TestingValues)
+            
+            var TestedValues = [[Double]](repeating: [Double](repeating: 0.0, count: 15), count: 15)
+            var I_test = Intensity
+            var Sigx_test = Sigmax
+            var Sigy_test = Sigmay
+            var x0_test = x_0
+            var y0_test = y_0
+            var A_test = A
+            var B_test = B
+            var SumArray = Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: Array(repeating: 0.0, count: 3), count: 3), count: 3), count: 3), count: 3), count: 3), count: 3)
+
+            for I in 0...2{
+                for J in 0...2{
+                    for K in 0...2{
+                        for L in 0...2{
+                            for M in 0...2{
+                                for N in 0...2{
+                                    for O in 0...2{
+                                        var sum = 0.0
+                                        I_test = Intensity
+                                        Sigx_test = Sigmax
+                                        Sigy_test = Sigmay
+                                        x0_test = x_0
+                                        y0_test = y_0
+                                        A_test = A
+                                        B_test = B
+                                        // Sigx_test = Sigmax + (Double(J - 1) * k * Sigmax)
+                                        //  Sigy_test = Sigmay + (Double(L - 1) * l * Sigmay)
+                                        // I_test  = Intensity + (Double(I - 1) * h * Intensity)
+                                        // x0_test = x_0 + (Double(K - 1) * f * x_0)
+                                        //y0_test = y_0 + (Double(M - 1) * m * x_0)
+                                        //A_test = A + (Double(N - 1) * n * A)
+                                        // B_test = B + (Double(O - 1) * o * B)
+                                        I_test  = Intensity + (Double(I - 1) * Istep)
+                                        Sigx_test = Sigmax + (Double(J - 1) * Sxstep)
+                                        Sigy_test = Sigmay + (Double(L - 1) * Systep)
+                                        x0_test = x_0 + (Double(K - 1) * x0step)
+                                        y0_test = y_0 + (Double(M - 1) * y0step)
+                                        A_test = A + (Double(N - 1) * Astep )
+                                        B_test = B + (Double(O - 1) * Bstep)
+
+                                        //no idea how this program would react to trying to creat a gaussian profile near the edge of a photo, because you would have to only fit half a gaussian. should probably discard star candidates within 7-8 pixels of the edge.
+                                        for x in 0..<width{
+                                            for y in 0..<height{
+                                                let term1 = pow((Double(x)-x0_test),2.0)/pow(Sigx_test,2.0)
+                                                let term2 = pow((Double(y)-y0_test),2.0)/pow(Sigy_test,2.0)
+                                                let exponential = I_test * exp(-(term1+term2))
+                                                let intensity = exponential + (A_test * (Double(x)-x0_test)) + (B_test * (Double(y)-y0_test))
+                                                //this is the array that will become graphed.
+                                                TestedValues[x][y]=intensity
+                                                sum = sum + pow((TestingValues[x][y] - TestedValues[x][y]),2)
+                                            }
+                                        }
+                                        // print(I_test,Sig_test, sum)
+                                        //I, sx, x0, sy, y0, a, b
+                                        SumArray[I][J][K][L][M][N][O] = sum
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            
             numloop = numloop + 1
             print(Intensity, Sigmax, x_0, y_0, Sigmay, A, B) //this keeps running for some reason???
             
             //tolerance is controlled here, tolerance value needs to increase (accuracy decrease) with more variables.
-            if SumArray[1][1][1][1][1][1][1] <= 0.005{
+            if SumArray[1][1][1][1][1][1][1] <= 0.008{
                 minimized = false
                  foundParameters = []
                 Istring = String(format: "central intensity= %.2f.", Intensity)
@@ -137,16 +214,17 @@ class GaussianFinder: ObservableObject {
             if I_derivative < 0 {
                 Intensity = Intensity + h
             }
-            if sigx_derivative > 0{
+            if sigx_derivative > 0 {
                 Sigmax = Sigmax - k
             }
-            if sigx_derivative < 0{
+            if sigx_derivative < 0 {
                 Sigmax = Sigmax + k
             }
-            if sigy_derivative > 0{
+            
+            if sigy_derivative > 0 {
                 Sigmay = Sigmay - l
             }
-            if sigy_derivative < 0{
+            if sigy_derivative < 0 {
                 Sigmay = Sigmax + l
             }
             if x_derivative > 0{
